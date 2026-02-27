60c6ac5ee283ff1e941da0eaa99cb0d1fb6ecdee
yito88 <yito88@protonmail.com>
2026-02-27 22:58:31 +0100

    upgrade clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/yito88/scarab"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
                  [com.scalar-labs/scalardb "3.17.1"]]
   :test-selectors {:default (complement :integration)
                    :integration :integration})
